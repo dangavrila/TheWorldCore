5790e2e03594ca9c3419840306c689fd85aef203
DanGavrila <dan.gavrila3@gmail.com>
2017-09-04 22:46:22 +0300

    Basic JavaScript

---
+++ TheWorldCore/wwwroot/js/site.js
@@ -0,0 +1,12 @@
+﻿// site.js
+var ele = document.getElementById("username");
+ele.innerHTML = "Anne Christie";
+
+var main = document.getElementById("main");
+main.onmouseenter = function () {
+    main.style = " background: #888";
+};
+
+main.onmouseleave = function () {
+    main.style = "";
+};
