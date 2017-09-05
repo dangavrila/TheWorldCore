8e4893e7a217ffc64c07f0367e3106db7cbd384e
DanGavrila <dan.gavrila3@gmail.com>
2017-09-05 22:31:08 +0300

    First, added jQuery package and integrate it. Second, migrate to Core SDK 2.0. Third, added MVC.

--- TheWorldCore/wwwroot/js/site.js
+++ TheWorldCore/wwwroot/js/site.js
@@ -1,14 +1,32 @@
 ﻿// site.js
 (function startup() {
-    var ele = document.getElementById("username");
-    ele.innerHTML = "Anne Christie";
+    //var ele = $("#username");
+    //ele.text("Anne Christie");
 
-    var main = document.getElementById("main");
-    main.onmouseenter = function () {
-        main.style = " background: #888";
-    };
+    //var main = $("#main");
+    //main.mouseenter(function () {
+    //    main.css("background-color", "#888");
+    //});
 
-    main.onmouseleave = function () {
-        main.style = "";
-    };
+    //main.mouseleave(function () {
+    //    main.css("background-color", "#eee");
+    //});
+
+    //var menuItems = $("ul.menu li a");
+    //menuItems.on("click", function () {
+    //    var me = $(this);
+    //    alert(me.text());
+    //})
+
+
+    var $sidebarAndWrapper = $("#sidebar,#wrapper"); // jQuerry wrap set
+    $("#sidebarToggle").on("click", function () {
+        $sidebarAndWrapper.toggleClass("hide-sidebar");
+        if ($sidebarAndWrapper.hasClass("hide-sidebar")) {
+            $(this).text("Show Sidebar");
+        } else {
+            $(this).text("Hide Sidebar");
+        }
+    });
+
 }) ();
